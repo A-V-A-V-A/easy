3e13b7397fa788a283e5518295320dad556fda45
A V A <ava@As-MacBook-Pro.local>
2018-12-09 10:46:41 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/easy/precache-manifest.db4bc056ca7614b2cd9be4e679074603.js"
+  "/easy.io/precache-manifest.511aaae8a03f4d91a570375f4bafc7e0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/easy/index.html", {
+workbox.routing.registerNavigationRoute("/easy.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
